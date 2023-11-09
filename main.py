d985a7a77f73c9a48834900d4c437135a10f4741
Pedro Ikeda <pedroikeda1@gmail.com>
2023-11-09 12:21:45 -0300

    added menu

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame as pg
 import sys
 from os import path
-from settings import *
+import settings as s
+# from settings import *
 from sprites import *
+import time
+
 
 class Game:
     def __init__(self):
@@ -11,20 +14,22 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         pg.key.set_repeat(500, 100)
-        self.load_data()
 
-    def load_data(self):
+    def load_data(self, map):
         game_folder = path.dirname(__file__)
         self.map_data = []
-        with open(path.join(game_folder, 'map2.txt'), 'rt') as f:
+        with open(path.join(game_folder, map), 'rt') as f:
             for line in f:
                 self.map_data.append(line)
 
     def new(self):
         # initialize all variables and do all the setup for a new game
+        self.load_data(f"map{s.MAPNUM}.txt")
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
         self.items = pg.sprite.Group()
+        self.startTime = time.time()
+        print(f"Start Time: {self.startTime}")
         for row, tiles in enumerate(self.map_data):
             for col, tile in enumerate(tiles):
                 if tile == '1':
@@ -50,6 +55,12 @@ class Game:
     def update(self):
         # update portion of the game loop
         self.all_sprites.update()
+        self.tmpTime = time.time()
+        self.currTime = abs((self.startTime - self.tmpTime))
+        print(f"current time: {self.currTime:.2f}")
+
+        if self.currTime >= s.TIMER:
+            self.playing = False
 
     # def draw_grid(self):
     #     for x in range(0, WIDTH, TILESIZE):
@@ -72,16 +83,217 @@ class Game:
                 if event.key == pg.K_ESCAPE:
                     self.quit()
 
-    def show_start_screen(self):
-        pass
+    def GOevents(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    s.MAPNUM+=1
+                    self.GO_screen = False
+    
+
+    def hover_check(self,img):
+        return img.rect.collidepoint(pg.mouse.get_pos())
+
+    def main_menu(self):
+
+        # pg.mixer.music.play()
+        self.menu_loop = True
+
+        while self.menu_loop:
+            
+            self.screen.blit(s.BGMENU,[0, 0])
+
+            mm_draw_group1.draw(self.screen)
+
+            if self.hover_check(start_btn1):
+                mm_start.draw(self.screen)
+            elif self.hover_check(option_btn1):
+                mm_option.draw(self.screen)
+            elif self.hover_check(exit_btn1):
+                mm_quit.draw(self.screen)
+
+            for event in pg.event.get():
+
+                if event.type == pg.QUIT:
+                    self.quit()
+
+                elif event.type == pg.MOUSEMOTION:
+                    x = pg.mouse.get_pos()[0]
+                    y = pg.mouse.get_pos()[1]
+                    # print(x, y)
+
+                elif (event.type == pg.MOUSEBUTTONDOWN):
+                    print(x, y)
+                    # Start
+                    if x >= startX and x <= startX+buttonW and y >= startY and y <= startY+buttonH:
+                        click_snd.play()
+                        self.screen.blit(start_btn3,[startX, startY])
+                        pg.display.update()
+                        pg.time.wait(400)
+                        self.screen.fill(BLACK)
+                        pg.display.update()
+                        pg.time.wait(1000)
+                        self.menu_loop = False
+                    
+                    # Options
+                    elif x >= optionX and x <= optionX+buttonW and y >= optionY and y <= optionY+buttonH:
+                        click_snd.play()
+                        self.screen.blit(option_btn3,[optionX, optionY])
+                        pg.display.update()
+                        pg.time.wait(400)
+                        self.option_screen()
+                    
+                    # Quit
+                    elif x >= exitX and x <= exitX+buttonW and y >= exitY and y <= exitY+buttonH:
+                        click_snd.play()
+                        self.screen.blit(exit_btn3,[exitX, exitY])
+                        pg.display.update()
+                        pg.time.wait(400)
+                        self.quit()
+                    
+
+            pg.display.update()
+
+    def option_screen(self):
+        self.vol = 0.5
+        self.vol_msc = 0.05
+        self.options = True
+        self.snd_state = 1
+
+        while self.options:
+            screen.fill(WHITE)
+            option_draw_group.draw(screen)
+            close_draw_group.draw(screen)
+            # print(self.snd_state,self.vol, self.vol_msc)
+
+            font = pg.font.Font('freesansbold.ttf', 32)
+ 
+            # create a text surface object,
+            # on which text is drawn on it.
+            text = font.render(str(f"{self.vol:.1f}"), True, BLACK)
+            
+            # create a rectangular object for the
+            # text surface object
+            textRect = text.get_rect()
+            
+            # set the center of the rectangular object.
+            textRect.center = ((WIDTH // 2), (HEIGHT // 2) + 100)
+
+            if self.hover_check(close_btn1):
+                close_hover.draw(screen)
+            if self.hover_check(snd_add):
+                snd_add_hover.draw(screen)
+            if self.hover_check(snd_minus):
+                snd_minus_hover.draw(screen)
+            if self.snd_state == 1:
+                snd_on_button.draw(screen)
+                if self.hover_check(snd_on):
+                    snd_on_hover.draw(screen)
+            if self.snd_state == 0:
+                snd_off_button.draw(screen)
+                if self.hover_check(snd_mute):
+                    snd_off_hover.draw(screen)
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                elif event.type == pg.MOUSEMOTION:
+                    x = pg.mouse.get_pos()[0]
+                    y = pg.mouse.get_pos()[1]
+                    # print(x, y)
+                elif event.type == pg.MOUSEBUTTONDOWN:
+                    x = pg.mouse.get_pos()[0]
+                    y = pg.mouse.get_pos()[1]
+
+                    # close
+                    if (x >= closeX and x <= closeX+closeW and y >= closeY and y <= closeY+closeH):
+                        click_snd.play()
+                        pg.time.wait(400)
+                        self.options = False
+
+                    # Minus
+                    elif (x >= (WIDTH/2)-125 and x <= (WIDTH/2)-75 and y >= (HEIGHT/2) and y <= (HEIGHT/2)+50):
+                        click_snd.play()
+                        screen.blit(snd_minus_c, [(WIDTH/2)-125, (HEIGHT/2)])
+                        pg.display.update()
+                        pg.time.wait(200)
+                        if self.vol > 0 and self.vol_msc > 0:
+                            self.vol -= 0.1
+                            self.vol_msc -= 0.01
+                            click_snd.set_volume(self.vol)
+                            pg.mixer.music.set_volume(self.vol_msc)
+                            item_snd.set_volume(self.vol)
+                            if self.vol <= 0.1 and self.vol_msc <= 0.01:
+                                self.snd_state = 0
+                                self.vol = 0
+                                self.vol_msc = 0
+                    
+                    # Plus
+                    elif (x >= (WIDTH/2)+75 and x <= (WIDTH/2)+125 and y >= (HEIGHT/2) and y <= (HEIGHT/2)+50):
+                        click_snd.play()
+                        screen.blit(snd_add_c, [(WIDTH/2)+75, (HEIGHT/2)])
+                        pg.display.update()
+                        pg.time.wait(200)
+                        if self.vol < 0.9:
+                            self.vol += 0.1
+                            self.vol_msc += 0.01
+                        click_snd.set_volume(self.vol)
+                        pg.mixer.music.set_volume(self.vol_msc)
+                        item_snd.set_volume(self.vol)
+                        if self.vol > 0 and self.vol_msc > 0:
+                            self.snd_state = 1
+                        if self.vol >= 1 and self.vol_msc >= 1:
+                            self.snd_state = 1
+                            self.vol = 1
+                            self.vol_msc = 1
+                            
+
+                    # On
+                    elif self.snd_state == 1:
+                        if (x >= (WIDTH/2)-25 and x <= (WIDTH/2)+25 and y >= (HEIGHT/2) and y <= (HEIGHT/2)+50):
+                            click_snd.play()
+                            screen.blit(snd_on_c, [(WIDTH/2)-25, (HEIGHT/2)])
+                            pg.display.update()
+                            pg.time.wait(200)
+                            self.vol = 0
+                            self.vol_msc = 0
+                            click_snd.set_volume(self.vol)
+                            pg.mixer.music.set_volume(self.vol_msc)
+                            item_snd.set_volume(self.vol)
+                            self.snd_state = 0
+
+                    # Off
+                    elif self.snd_state == 0:
+                        if (x >= (WIDTH/2)-25 and x <= (WIDTH/2)+25 and y >= (HEIGHT/2) and y <= (HEIGHT/2)+50):
+                            click_snd.play()
+                            screen.blit(snd_mute_c, [(WIDTH/2)-25, (HEIGHT/2)])
+                            pg.display.update()
+                            pg.time.wait(200)
+                            self.vol = 0.5
+                            self.vol_msc = 0.05
+                            click_snd.set_volume(self.vol)
+                            pg.mixer.music.set_volume(self.vol_msc)
+                            item_snd.set_volume(self.vol)
+                            self.snd_state = 1
+            screen.blit(text, textRect)
+            pg.display.update()
 
     def show_go_screen(self):
-        pass
+        self.GO_screen = True
+
+        while self.GO_screen:
+            self.screen.fill(RED)
+            pg.display.flip()
+            self.GOevents()
 
 # create the game object
 g = Game()
-g.show_start_screen()
+# g.show_start_screen()
 while True:
+    g.main_menu()
     g.new()
     g.run()
     g.show_go_screen()
--- settings.py
+++ settings.py
@@ -1,9 +1,12 @@
 import pygame as pg
 
-wallFront = pg.image.load = ('wall_front.png')
+pg.init()
+pg.mixer.init()
 
 
+TIMER = 5
 SCORE = 0
+MAPNUM = 2
 
 # define some colors (R, G, B)
 WHITE = (255, 255, 255)
@@ -27,3 +30,133 @@ GRIDWIDTH = WIDTH / TILESIZE
 GRIDHEIGHT = HEIGHT / TILESIZE
 
 PLAYER_SPEED = 350
+
+screen = pg.display.set_mode([WIDTH, HEIGHT])
+
+# function to create rect and scale image
+def make_rect(group, path, l, t, w, h,scale=False, nW=0, nH=0):
+    temp = pg.sprite.Sprite(group)
+    temp.image = pg.image.load(path).convert_alpha()
+    temp.rect = pg.Rect(l, t, w, h)
+    if scale == True:
+        temp.image = pg.transform.scale(temp.image, [nW, nH])
+
+    return temp
+
+# Draw groups
+## Menu
+mm_draw_group1 = pg.sprite.Group()
+mm_start = pg.sprite.Group()
+mm_option = pg.sprite.Group()
+mm_inst = pg.sprite.Group()
+mm_quit = pg.sprite.Group()
+## option
+option_draw_group = pg.sprite.Group()
+snd_add_hover = pg.sprite.Group()
+snd_minus_hover = pg.sprite.Group()
+snd_on_button = pg.sprite.Group()
+snd_on_hover = pg.sprite.Group()
+snd_off_button = pg.sprite.Group()
+snd_off_hover = pg.sprite.Group()
+# Close Button
+close_draw_group = pg.sprite.Group()
+close_hover = pg.sprite.Group()
+start_draw_group = pg.sprite.Group()
+
+
+# Loading images
+
+## Menu
+mm_draw_group1 = pg.sprite.Group()
+mm_start = pg.sprite.Group()
+mm_option = pg.sprite.Group()
+mm_inst = pg.sprite.Group()
+mm_quit = pg.sprite.Group()
+
+## option
+option_draw_group = pg.sprite.Group()
+snd_add_hover = pg.sprite.Group()
+snd_minus_hover = pg.sprite.Group()
+snd_on_button = pg.sprite.Group()
+snd_on_hover = pg.sprite.Group()
+snd_off_button = pg.sprite.Group()
+snd_off_hover = pg.sprite.Group()
+# Close Button
+close_draw_group = pg.sprite.Group()
+close_hover = pg.sprite.Group()
+start_draw_group = pg.sprite.Group()
+## Game Over
+go_draw_group = pg.sprite.Group()
+go_quit = pg.sprite.Group()
+go_retry = pg.sprite.Group()
+
+# Images
+BGMENU = pg.image.load("img\\menu\\main_menu\\restaurante.jpg").convert()
+BGMENU = pg.transform.scale(BGMENU, [WIDTH, HEIGHT])
+
+
+# mm_draw_group
+buttonW = 200
+buttonH = 50
+startX, startY = 60, 500
+start_btn1 = make_rect(mm_draw_group1, "img\\menu\\main_menu\\start1.png", startX, startY, buttonW, buttonH)
+start_btn2 = make_rect(mm_start, "img\\menu\\main_menu\\start2.png", startX, startY, buttonW, buttonH)
+start_btn3 = pg.image.load("img\\menu\\main_menu\\start3.png")
+
+optionX, optionY = startX, startY+60
+option_btn1 = make_rect(mm_draw_group1, "img\\menu\\main_menu\\options1.png", optionX, optionY, buttonW, buttonH)
+option_btn2 = make_rect(mm_option, "img\\menu\\main_menu\\options2.png", optionX, optionY, buttonW, buttonH)
+option_btn3 = pg.image.load("img\\menu\\main_menu\\options3.png")
+
+exitX, exitY = startX, optionY+60
+exit_btn1 = make_rect(mm_draw_group1, "img\\menu\\main_menu\\sair1.png", exitX, exitY, buttonW, buttonH)
+exit_btn2 = make_rect(mm_quit, "img\\menu\\main_menu\\sair2.png", exitX, exitY, buttonW, buttonH)
+exit_btn3 = pg.image.load("img\\menu\\main_menu\\sair3.png")
+
+closeW, closeH = 50, 50
+closeX, closeY = WIDTH-80, 30
+close_btn1 = make_rect(close_draw_group, "img\\menu\\botao_sair.png", closeX, closeY, closeW, closeH)
+close_btn2 = make_rect(close_hover, "img\\menu\\botao_sair2.png", closeX, closeY, closeW, closeH)
+
+# option_draw_group
+
+option_pag1 = make_rect(option_draw_group, "img\\menu\\options\\options_pag1.jpg", 0, 0, 732, 400, True, WIDTH, HEIGHT)
+
+snd_btn_H, snd_btn_W = 50, 50
+snd_on = make_rect(snd_on_button, "img\\menu\\options\\botao_som_on_02.png", (WIDTH/2)-25, (HEIGHT/2), snd_btn_H, snd_btn_W)
+snd_on_h = make_rect(snd_on_hover, "img\\menu\\options\\botao_som_on_03.png", (WIDTH/2)-25, (HEIGHT/2), snd_btn_H, snd_btn_W)
+snd_on_c = pg.image.load("img\\menu\\options\\botao_som_on.png")
+
+snd_minus = make_rect(option_draw_group, "img\\menu\\options\\botao_som_down_02.png", (WIDTH/2)-125, (HEIGHT/2), snd_btn_H, snd_btn_W)
+snd_minus_h = make_rect(snd_minus_hover, "img\\menu\\options\\botao_som_down_03.png", (WIDTH/2)-125, (HEIGHT/2), snd_btn_H, snd_btn_W)
+snd_minus_c = pg.image.load("img\\menu\\options\\botao_som_down.png")
+
+snd_add = make_rect(option_draw_group, "img\\menu\\options\\botao_som_up_02.png", (WIDTH/2)+75, (HEIGHT/2), snd_btn_H, snd_btn_W)
+snd_add_h = make_rect(snd_add_hover, "img\\menu\\options\\botao_som_up_03.png", (WIDTH/2)+75, (HEIGHT/2), snd_btn_H, snd_btn_W)
+snd_add_c = pg.image.load("img\\menu\\options\\botao_som_up.png")
+
+snd_mute = make_rect(snd_off_button, "img\\menu\\options\\botao_som_off_02.png", (WIDTH/2)-25, (HEIGHT/2), snd_btn_H, snd_btn_W)
+snd_mute_h = make_rect(snd_off_hover, "img\\menu\\options\\botao_som_off_03.png", (WIDTH/2)-25, (HEIGHT/2), snd_btn_H, snd_btn_W)
+snd_mute_c = pg.image.load("img\\menu\\options\\botao_som_off.png")
+
+
+# GO screen
+retry_btn1 = make_rect(go_draw_group, "img\\menu\\game_over\\restart1.png", 80, 260, buttonW, buttonH)
+retry_btn2 = make_rect(go_retry, "img\\menu\\game_over\\restart2.png", 80, 260, buttonW, buttonH)
+retry_btn3 = pg.image.load("img\\menu\\game_over\\restart3.png")
+quit_btn1 = make_rect(go_draw_group, "img\\menu\\main_menu\\sair1.png", 480, 260, buttonW, buttonH)
+quit_btn2 = make_rect(go_quit, "img\\menu\\main_menu\\sair2.png", 480, 260, buttonW, buttonH)
+quit_btn3 = pg.image.load("img\\menu\\main_menu\\sair3.png")
+
+
+# Sound
+vol = 0.5
+vol_msc = 0.05
+click_snd = pg.mixer.Sound("snd\\Effects\\btn_click.ogg")
+click_snd.set_volume(vol)
+
+# music = pg.mixer.music.load("snd\\BGM\\")
+# pg.mixer.music.set_volume(vol_msc)
+
+item_snd = pg.mixer.Sound("snd\\Effects\\item.ogg")
+item_snd.set_volume(vol)
